bc98a57fb1808ccc9c384ec8a079178a23094b01
Zane Paton <zanerepaton@gmail.com>
2020-01-31 10:22:39 -0600

    Updates

--- precache-manifest.4aefe7beb49cf365e3fe92657f234de9.js
+++ precache-manifest.4aefe7beb49cf365e3fe92657f234de9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7114bd1ed019d9d31216d20e4bc5e2fc",
+    "revision": "fa01e3baa6920953da08234d0a1e21b9",
     "url": "/SD260-Final-Project/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/SD260-Final-Project/static/css/2.1a08bad1.chunk.css"
   },
   {
-    "revision": "b219f86293b1a8c5ede7",
+    "revision": "7d8ba55acefaa6fe29c7",
     "url": "/SD260-Final-Project/static/css/main.01db78e1.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/SD260-Final-Project/static/js/2.781ff533.chunk.js.LICENSE"
   },
   {
-    "revision": "b219f86293b1a8c5ede7",
-    "url": "/SD260-Final-Project/static/js/main.3f574d96.chunk.js"
+    "revision": "7d8ba55acefaa6fe29c7",
+    "url": "/SD260-Final-Project/static/js/main.9f3cb091.chunk.js"
   },
   {
     "revision": "26337ded127fea3fdfb9",
