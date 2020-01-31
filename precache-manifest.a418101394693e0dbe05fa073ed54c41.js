66f368b1b6fa88fd14ff433f8f53a87da1462f5e
Zane Paton <zanerepaton@gmail.com>
2020-01-31 09:25:08 -0600

    Updates

---
+++ precache-manifest.a418101394693e0dbe05fa073ed54c41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59cc84234fad942d38c025c9e3d4f3f6",
+    "url": "/SD260-Final-Project/index.html"
+  },
+  {
+    "revision": "f053dcbf01373be8f70c",
+    "url": "/SD260-Final-Project/static/css/2.1a08bad1.chunk.css"
+  },
+  {
+    "revision": "ecf6af3326f80a36faf2",
+    "url": "/SD260-Final-Project/static/css/main.01db78e1.chunk.css"
+  },
+  {
+    "revision": "f053dcbf01373be8f70c",
+    "url": "/SD260-Final-Project/static/js/2.781ff533.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SD260-Final-Project/static/js/2.781ff533.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ecf6af3326f80a36faf2",
+    "url": "/SD260-Final-Project/static/js/main.e432078a.chunk.js"
+  },
+  {
+    "revision": "26337ded127fea3fdfb9",
+    "url": "/SD260-Final-Project/static/js/runtime-main.38aee715.js"
+  }
+]);
